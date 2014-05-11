c7d8c4dfc882e7ca16a713e1e8362537935404bc
Anantha Kumaran <ananthakumaran@gmail.com>
2014-05-11 10:24:21 +0530

    initial commit

---
+++ test/recordTest.js
@@ -0,0 +1,42 @@
+var Record = require('../lib'),
+	Q = require('q'),
+	_ = require('underscore'),
+	expect = require('expect.js'),
+	request = require('request');
+
+var fixture = __dirname + '/../fixture/record';
+
+describe('record', function () {
+	this.timeout(10000);
+
+	var requesting = function () {
+		return Q.all(_.map(_.range(10), function (i) {
+			return Q.nfcall(request, 'http://echo.jsontest.com/id/' + i);
+		}));
+	};
+
+	var checkResponses = function (responses) {
+		var bodies = _.map(responses, function (response) { return JSON.parse(response[1]); });
+		expect(bodies).to.eql(_.map(_.range(10), function (i) {
+			return {id: i.toString()};
+		}));
+	};
+
+	it('should record', function (done) {
+		var record = new Record(fixture);
+		record.start('record');
+		requesting().then(function (responses) {
+			record.stop();
+			checkResponses(responses);
+		}).nodeify(done);
+	});
+
+	it('should replay', function (done) {
+		var record = new Record(fixture);
+		record.start('replay');
+		requesting().then(function (responses) {
+			record.stop();
+			checkResponses(responses);
+		}).nodeify(done);
+	});
+});
